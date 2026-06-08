60f6d87de5f6164c62ab7f2b5117dbccb633a238
unknown <kn253_dis@student.ztu.edu.ua>
2026-06-08 20:31:16 +0300

    Work with DB

--- Banking system/Entity/User.cs
+++ Banking system/Entity/User.cs
@@ -5,25 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Banking_system.Entity
 {
     public class User
     {
         [Key]
-        private int ID { get; set; }
-        private string Password { get; set; } = string.Empty;
-        private string Email { get; set; } = string.Empty;
-        private string Name { get; set; } = string.Empty;
-        private string Surname { get; set; } = string.Empty;
-        private string MiddleName { get; set; } = string.Empty;
-        private string CardNumber { get; set; } = string.Empty;
+        public int ID { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Ipn { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
 
         public User() { }
 
-        public User(string password, string email, string name, string surname, string middleName,string cardNumber)
+        public User(string password, string phone, string email, string ipn, string name, string surname, string middleName, string cardNumber)
         {
             Password = password;
+            Phone = phone;
             Email = email;
+            Ipn = ipn;
             Name = name;
             Surname = surname;
             MiddleName = middleName;
