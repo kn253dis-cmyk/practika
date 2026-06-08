ecb81da76c8b7fade3d22240a860d5de30615583
unknown <kn253_dis@student.ztu.edu.ua>
2026-06-08 17:48:18 +0300

    Card System Add

--- Banking system/Models/AbstractCard.cs
+++ Banking system/Models/AbstractCard.cs
@@ -1,24 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Banking_system.Models
 {
     internal abstract class AbstractCard
     {
-        public string CardNumber { get; set; } = string.Empty;
-        private short CVV { get; set; } = short.MaxValue;
-        private DateTime ExpirationDate { get; set; } = DateTime.MaxValue;
+        public string CardNumber { get; protected set; } = string.Empty;
+        public decimal Balance { get; protected set; } = 0m;
+        private short CVV { get; set; }
+        private DateTime ExpirationDate { get; set; }
 
         public AbstractCard() { }
-        public AbstractCard(string cardNumber, short cVV, DateTime expirationDate)
+
+        public AbstractCard(string code)
         {
-            CardNumber = cardNumber;
-            CVV = cVV;
-            ExpirationDate = expirationDate;
+            CardNumber = GenerateCardNumber(code);
+            Random rnd = new Random();
+            ExpirationDate = DateTime.Now.AddYears(5);
+            CVV = (short)rnd.Next(100, 999);
+
+            Logger.Log($"Створено нову картку. Номер: {CardNumber}");
         }
 
-        public short GetCVV() { return CVV; }
-        public DateTime GetExpirationDate() { return ExpirationDate; }
+        public short GetCVV() => CVV;
+        public DateTime GetExpirationDate() => ExpirationDate;
+
+        public virtual void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Logger.Log($"Помилка поповнення ({CardNumber}): Сума має бути більшою за нуль.");
+                throw new ArgumentException("Сума поповнення має бути більшою за 0.");
+            }
+
+            Balance += amount;
+            Logger.Log($"Картку {CardNumber} поповнено на {amount:C}. Новий баланс: {Balance:C}");
+        }
+
+        public virtual bool Withdraw(decimal amount)
+        {
+            if (amount <= 0) return false;
+
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                Logger.Log($"З картки {CardNumber} знято {amount:C}. Залишок: {Balance:C}");
+                return true;
+            }
+
+            Logger.Log($"Відмова ({CardNumber}): Недостатньо коштів для зняття {amount:C}. Баланс: {Balance:C}");
+            return false;
+        }
+
+        protected string GenerateCardNumber(string cardTypeCode)
+        {
+            string countryCode = "38";
+            string bankCode = "7777";
+
+            Random rnd = new Random();
+            string uniqueAccountPart = rnd.Next(1000000, 9999999).ToString();
+            string first15Digits = countryCode + cardTypeCode + bankCode + uniqueAccountPart;
+            int checkDigit = CalculateLuhnCheckDigit(first15Digits);
+
+            return first15Digits + checkDigit.ToString();
+        }
+
+        private int CalculateLuhnCheckDigit(string number)
+        {
+            int sum = 0;
+            bool alternate = true;
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                int n = int.Parse(number[i].ToString());
+                if (alternate)
+                {
+                    n *= 2;
+                    if (n > 9) n -= 9;
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return (10 - (sum % 10)) % 10;
+        }
     }
 }
--- Banking system/Models/UniorCard.cs
+++ Banking system/Models/UniorCard.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Banking_system.Models
+﻿namespace Banking_system.Models
 {
     internal class UniorCard : AbstractCard
     {
+        public decimal TransactionLimit { get; set; } = 1000m;
+        public UniorCard() : base("20")
+        {
+            Logger.Log($"Ініціалізовано картку юніора {CardNumber} з лімітом операцій {TransactionLimit:C}.");
+        }
+        public override bool Withdraw(decimal amount)
+        {
+            if (amount > TransactionLimit)
+            {
+                Logger.Log($"Відмова ({CardNumber}): Спроба зняти {amount:C} перевищує встановлений ліміт для юніора ({TransactionLimit:C}).");
+                return false;
+            }
+            return base.Withdraw(amount);
+        }
     }
 }
