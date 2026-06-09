bce83bff09c3f218aeea30cb2a905448cb13e04a
unknown <kn253_dis@student.ztu.edu.ua>
2026-06-09 23:31:28 +0300

    Transaction improve

--- Banking system/Models/Transactions/TransferTransaction.cs
+++ Banking system/Models/Transactions/TransferTransaction.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Banking_system.Models.Transactions
+﻿namespace Banking_system.Models.Transactions
 {
-    class TransferTransaction
+    internal class TransferTransaction : AbstractTransaction
     {
+        private AbstractCard _sourceCard;
+        private AbstractCard _targetCard;
+
+        public TransferTransaction(AbstractCard sourceCard, AbstractCard targetCard, decimal amount) : base(amount)
+        {
+            _sourceCard = sourceCard;
+            _targetCard = targetCard;
+        }
+
+        public override bool Execute()
+        {
+            if (_sourceCard.CardNumber == _targetCard.CardNumber)
+            {
+                Logger.Log($"[Транзакція {TransactionId}] Помилка: Спроба переказу на ту саму картку ({_sourceCard.CardNumber}).");
+                return false;
+            }
+
+            Logger.Log($"[Транзакція {TransactionId}] Ініційовано переказ {Amount:C} з картки {_sourceCard.CardNumber} на {_targetCard.CardNumber}.");
+
+            bool isWithdrawn = _sourceCard.Withdraw(Amount);
+
+            if (isWithdrawn)
+            {
+                _targetCard.Deposit(Amount);
+                Logger.Log($"[Транзакція {TransactionId}] Переказ успішно завершено.");
+                return true;
+            }
+            else
+            {
+                Logger.Log($"[Транзакція {TransactionId}] Переказ скасовано через проблеми зі зняттям коштів з картки {_sourceCard.CardNumber}.");
+                return false;
+            }
+        }
     }
 }
--- Banking system/Models/Transactions/WithdrawalTransaction.cs
+++ Banking system/Models/Transactions/WithdrawalTransaction.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Banking_system.Models.Transactions
+﻿namespace Banking_system.Models.Transactions
 {
-    class WithdrawalTransaction
+    internal class WithdrawalTransaction : AbstractTransaction
     {
+        private AbstractCard _sourceCard;
+
+        public WithdrawalTransaction(AbstractCard sourceCard, decimal amount) : base(amount)=>_sourceCard = sourceCard;
+        public override bool Execute()
+        {
+            Logger.Log($"[Транзакція {TransactionId}] Запит на зняття {Amount:C} з картки {_sourceCard.CardNumber}.");
+
+            bool success = _sourceCard.Withdraw(Amount);
+
+            if (success)
+                Logger.Log($"[Транзакція {TransactionId}] Успішно знято {Amount:C} з картки {_sourceCard.CardNumber}.");
+            else
+                Logger.Log($"[Транзакція {TransactionId}] Відхилено. Недостатньо коштів або перевищено ліміт на картці {_sourceCard.CardNumber}.");
+
+            return success;
+        }
     }
 }
